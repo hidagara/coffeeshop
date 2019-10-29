91d00e8930415f2a50bb90033bf37dd67df0e76e
hidagara <hidagara@gmail.com>
2019-10-29 22:21:40 +0300

    store collection view cell

---
+++ coffeeshop/UI/Shared/Cells/CollectionView/StoreCollectionViewCell/StoreCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StoreollectionViewCell.swift
+//  coffeeshop
+//
+//  Created by Roman Guseynov on 29.10.2019.
+//  Copyright © 2019 Roman Guseynov. All rights reserved.
+//
+
+import UIKit
+
+class StoreCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
