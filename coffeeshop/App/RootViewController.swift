78723a3ea4f34d5fca166df88a8623ad97ca41b7
hidagara <hidagara@gmail.com>
2020-03-15 20:46:24 +0300

    hola

--- coffeeshop/App/RootViewController.swift
+++ coffeeshop/App/RootViewController.swift
@@ -15,7 +15,7 @@ class RootViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view
     self.view.backgroundColor = .white
-    
+    print ("hoola hola espaniola")
   }
   
   
